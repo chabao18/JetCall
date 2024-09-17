42b1107f1a57176c8cc024347735ea50fc1e2d6c
Zhixiong Lu <zhixiong.lu@qq.com>
2024-09-17 22:20:06 +0800

    Add common modules

---
+++ common/src/main/java/com/chabao18/common/service/UserService.java
@@ -0,0 +1,15 @@
+package com.chabao18.common.service;
+
+import com.chabao18.common.model.User;
+
+/**
+ * 用户服务
+ */
+public interface UserService {
+
+    /**
+     * @param user
+     * @return
+     */
+    User getUser(User user);
+}
