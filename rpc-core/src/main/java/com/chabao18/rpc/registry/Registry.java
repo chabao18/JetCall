766bf44d8bb9f97170350c1f7b833c46795e3792
Zhixiong Lu <zhixiong.lu@qq.com>
2025-01-09 19:00:46 +0800

    modify comments

--- rpc-core/src/main/java/com/chabao18/rpc/registry/Registry.java
+++ rpc-core/src/main/java/com/chabao18/rpc/registry/Registry.java
@@ -7,13 +7,7 @@ import com.chabao18.rpc.model.ServiceMetaInfo;
 
 import java.util.List;
 
-/**
- * 注册中心
- *
- * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
- * @learn <a href="https://codefather.cn">编程宝典</a>
- * @from <a href="https://yupi.icu">编程导航知识星球</a>
- */
+
 public interface Registry {
 
     void init(RegistryConfig registryConfig);
